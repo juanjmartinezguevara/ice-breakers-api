e1978fb4cac60f2c87dad41432da1980da4787f0
Juan J. Martinez-Guevara <76273125+juanjmartinezguevara@users.noreply.github.com>
2022-02-10 23:10:20 -0800

    Create Header, add Header styling

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+    return (
+        <nav className='header'>
+            <h1 className='header-title'>Icebreakers</h1>
+        </nav>
+    )
+}
+
+export default Header
