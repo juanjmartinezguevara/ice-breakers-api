963325e36e05c4e2c35dac46b3769cc5b397b057
Juan J. Martinez-Guevara <76273125+juanjmartinezguevara@users.noreply.github.com>
2022-02-17 18:35:49 -0600

    Fix Main container styling, create temporary icebreakers database

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,27 +1,26 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./Main.css";
+import Temp from './temp.json'
 
 function Main() {
   const [ib, setIb] = useState("Click the button below to get started");
 
-  const array = ["Ice-breaker 1", "Ice-breaker 2", "Ice-breaker 3"];
-
-  useEffect(() => {
-    axios
-      .get(
-        "https://graph.facebook.com/v13.0/me/messenger_profile?fields=ice_breakers&access_token=<PAGE_ACCESS_TOKEN>"
-      )
-      .then((res) => {
-        console.log(res.data);
-        setIb(res.data);
-      });
-  }, []);
+  // useEffect(() => {
+  //   axios
+  //     .get(
+  //       "https://graph.facebook.com/v13.0/me/messenger_profile?fields=ice_breakers&access_token=<PAGE_ACCESS_TOKEN>"
+  //     )
+  //     .then((res) => {
+  //       console.log(res.data);
+  //       setIb(res.data);
+  //     });
+  // }, []);
 
   const getIcebreaker = () => {
     return (
       <div>
-        <h1>{setIb(array[Math.floor(Math.random() * array.length)])}</h1>
+        <h1>{setIb(Temp[Math.floor(Math.random() * Temp.length)].question)}</h1>
       </div>
     );
   };
@@ -37,7 +36,7 @@ function Main() {
           Find an Ice Breaker
         </button>
       </div>
-      <span className='main-container main-container-divide'></span>
+      <span id='main-container-divide' className='main-container'></span>
       <div className="main-signup main-container">
           <h1>Want to submit your own icebreakers?</h1>
           <button className="main-button">Sign Up!</button>
